ac0b7a080660fb6de69a07cc72befce328734bb2
Leonardo Kriswanto <leon12okto@gmail.com>
2020-05-19 22:05:13 +0700

    tambah search sama icon + benahin biar center nya responsive

--- edit.php
+++ edit.php
@@ -39,11 +39,12 @@ while($databarang_data = mysqli_fetch_array($result))
     <title>Edit Data Barang</title>
     <link rel="stylesheet" type="text/css" href="bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 
 <body>
     <div class="container">
-        <a class="btn btn-primary" href="show.php">List Data Barang</a>
+        <a class="btn btn-primary" href="show.php"><i class="fa fa-list" aria-hidden="true"></i> Daftar Barang</a>
         <br/>
         <h1>Edit Data Barang </h1>
         <br/>
--- index.php
+++ index.php
@@ -4,10 +4,11 @@
         <title>Input Form</title>
         <link rel="stylesheet" type="text/css" href="bootstrap.min.css">
         <link rel="stylesheet" type="text/css" href="style.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     </head>
     <body>
         <div class="container">
-            <a href="show.php" class="btn btn-primary">Daftar Barang</a>
+            <a class="btn btn-primary" href="show.php"><i class="fa fa-list" aria-hidden="true"></i> Daftar Barang</a>
             <h1>Input Data Barang</h1>
             </br>
                 <form method="post" action="index.php">
